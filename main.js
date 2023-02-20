e2f0f2be19914a382f386b6eb3831d34dd9b1b1f
NazimAli28 <nazimalionline@gmail.com>
2023-02-20 19:50:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function add() {
+    let add1 = parseInt(document.getElementById("add1").value);
+    let add2 = parseInt(document.getElementById("add2").value);
+    let Add = add1 + add2;
+    alert(Add);
+}
+function sub() {
+    let sub1 = parseInt(document.getElementById("sub1").value);
+    let sub2 = parseInt(document.getElementById("sub2").value);
+    let Sub = sub1 - sub2;
+    alert(Sub);
+}
+function mul() {
+    let mul1 = parseInt(document.getElementById("mul1").value);
+    let mul2 = parseInt(document.getElementById("mul2").value);
+    let Mul = mul1 * mul2;
+    alert(Mul);
+}
+function div() {
+    let div1 = parseInt(document.getElementById("div1").value);
+    let div2 = parseInt(document.getElementById("div2").value);
+    let Div = div1 / div2;
+    alert(Div);
+}
